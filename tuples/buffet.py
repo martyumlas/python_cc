f15941543b90657eec7b60e877571dcbace33add
Marty Umlas <martyumlas@gmail.com>
2024-02-23 23:40:53 +0800

    tuples

---
+++ tuples/buffet.py
@@ -0,0 +1,9 @@
+buffet = ('menudo', 'dinuguan', 'caldereta', 'tuna', 'pampano')
+
+for food in buffet:
+    print(food)
+
+buffet = ('menudo', 'pampano', 'sarsiado', 'asado')
+
+for food in buffet:
+    print(food)
