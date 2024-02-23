19cdb7323e35b3d3ef42a228041a7c78b1f6ac74
Marty Umlas <martyumlas@gmail.com>
2024-02-23 23:34:35 +0800

    another list update

---
+++ list/slice.py
@@ -0,0 +1,12 @@
+players = ['marlon', 'jackie', 'ruben', 'rico', 'jonah']
+
+copy = players[:]
+
+players.append('mike')
+copy.append('gorio')
+
+print(players)
+print(copy)
+print(copy[2:])
+print(copy[:2])
+print(copy[1:4])
