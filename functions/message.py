9c0881a041d1e4fa00329556df9924905a291496
Marty Umlas <martyumlas@gmail.com>
2024-02-26 20:34:11 +0800

    initial functions

---
+++ functions/message.py
@@ -0,0 +1,4 @@
+def display_message():
+    print('I am learning about Python\'s function')
+
+display_message()
---
+++ functions/tshirt.py
@@ -0,0 +1,8 @@
+def make_shirt(size='large', text='I love Python'):
+    print(f'size: {size}')
+    print(f'message: {text}')
+
+
+make_shirt('medium', 'must be delivered today')
+make_shirt(text = 'this is old shirt', size='small')
+make_shirt()
