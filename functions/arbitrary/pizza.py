f2eaad8566ff8e4005e7acea5efc35a37d23672a
Marty Umlas <martyumlas@gmail.com>
2024-02-26 20:35:13 +0800

    initial arbitrary parameters

---
+++ functions/arbitrary/pizza.py
@@ -0,0 +1,6 @@
+'''this will return a tuple'''
+def make_pizza(size, *toppings):
+    print(size, toppings) 
+
+make_pizza('large', 'pepperoni')
+# # make_pizza('mushrooms', 'green pepppers', 'extra cheese')
