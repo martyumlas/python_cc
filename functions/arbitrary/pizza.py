37ef4f1f7e4b12d0b64183d4c709c21931cb1458
Marty Umlas <martyumlas@gmail.com>
2024-02-28 19:12:26 +0800

    initial artibtrary and modules

--- functions/arbitrary/pizza.py
+++ functions/arbitrary/pizza.py
@@ -2,5 +2,8 @@
 def make_pizza(size, *toppings):
     # print(f'{size} pizza with {",".join(toppings[:-1])} and {toppings[-1]}') 
     print(toppings)
-make_pizza('large', 'pepperoni', 'mushrooms', 'green pepppers', 'extra cheese')
+# make_pizza('large', 'pepperoni', 'mushrooms', 'green pepppers', 'extra cheese')
 # # make_pizza('mushrooms', 'green pepppers', 'extra cheese')
+
+def other_function_in_this_file():
+    print('Hello from /arbitrary/pizza')
---
+++ functions/modules/making_pizza.py
@@ -0,0 +1,7 @@
+import pizza
+# from pizza import other_function
+
+
+pizza.make_pizza(12, 'mushroom', 'pepperoni')
+pizza.other_function()
+
