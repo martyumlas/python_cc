d5d9318737ec7da7eaa0ed922af3f775660c121e
martyumlas <martyumlas@gmail.com>
2024-02-28 21:39:39 +0800

    args and kwargs

--- functions/arbitrary/pizza.py
+++ functions/arbitrary/pizza.py
@@ -1,9 +1,9 @@
 '''this will return a tuple'''
 def make_pizza(size, *toppings):
-    # print(f'{size} pizza with {",".join(toppings[:-1])} and {toppings[-1]}') 
+    print(f'{size} pizza with {",".join(toppings[:-1])} and {toppings[-1]}') 
     print(toppings)
-# make_pizza('large', 'pepperoni', 'mushrooms', 'green pepppers', 'extra cheese')
-# # make_pizza('mushrooms', 'green pepppers', 'extra cheese')
+make_pizza('large', 'pepperoni', 'mushrooms', 'green pepppers', 'extra cheese')
+make_pizza('mushrooms', 'green pepppers', 'extra cheese')
 
 def other_function_in_this_file():
     print('Hello from /arbitrary/pizza')
---
+++ functions/keywords_argument/profile.py
@@ -0,0 +1,8 @@
+def build_profile(first, last, **user_info):
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('marty', 'umlas', profession='software engineer', company='guerilla 360')
+
+print(user_profile)
